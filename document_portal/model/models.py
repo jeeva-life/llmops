76172f19ee097e974a9f727b74b751991b0533a7
jeeva-life <kodalijeevan756@gmail.com>
2025-08-02 13:58:50 -0500

    added pydantic models

---
+++ document_portal/model/models.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field, RootModel
+from typing import Optional, List, Dict, Any, Union
+
+class Metadata(BaseModel):
+    """
+    Metadata for a document.
+    """
+    Summary: List[str] = Field(default_factory=list, description="Summary of the document")
+    Title: str
+    Author: str 
+    DateCreated: str
+    LastModifiedDate: str
+    Publisher: str
+    Language: str
+    PageCount: Union[int, str]
+    SentimentTone: str
+
+class ChangeFormat(BaseModel):
+    Page: str
+    changes:str
+
+class SummaryResponse(RootModel[list[ChangeFormat]]):
+    pass 
