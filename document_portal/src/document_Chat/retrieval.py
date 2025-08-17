2be46d3339f195e5117a4f5e6751c9e6b68460e5
jeeva-life <kodalijeevan756@gmail.com>
2025-08-17 16:53:44 -0500

    handled errors

--- document_portal/src/document_Chat/retrieval.py
+++ document_portal/src/document_Chat/retrieval.py
@@ -11,7 +11,7 @@ from langchain_community.vectorstores import FAISS
 from utils.model_loader import ModelLoader
 from exception.custom_exception import DocumentPortalException
 from logger import GLOBAL_LOGGER as log
-from prompt.prompt_library import PROMPT_REGISTRY
+from prompts.prompt_library import PROMPT_REGISTRY
 from model.models import PromptType
 
 
